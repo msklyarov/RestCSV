8de5df69e6160a46499324ee196a678425899649
Maxim Sklyarov <sklyarov.ma@gmail.com>
2017-06-13 09:20:26 +0300

    Commit 9:20

--- csvServer.js
+++ csvServer.js
@@ -1,67 +1,67 @@
+const request = require('request');
 const csv = require('ya-csv');
-const config = require('./config');
 const Promise = require('bluebird');
 const mongoClient = Promise.promisifyAll(require('mongodb').MongoClient);
+const config = require('./config');
+const app = require('express')();
+
+// const strm = request
+//   .get({
+//     url: 'https://resources.lendingclub.com/SecondaryMarketAllNotes.csv',
+//     encoding: 'utf-8'
+//   });
 
 // temporary
 const fs = require('fs');
-const reader = csv.createCsvFileReader(config.csvFileUrl);
+
+//const reader = new csv.CsvReader(strm, {columnsFromHeader: true});
+const reader = csv.createCsvFileReader(config.csvFileUrl, {columnsFromHeader: true});
 
 let firstLine = true;
-let bulk = col.initializeUnorderedBulkOp();
+//let bulk = col.initializeUnorderedBulkOp();
 let counter = 0;
-reader.addListener('data', (data) => {
+reader.on('data', (data) => {
   if (firstLine) {
     firstLine = false;
   } else {
     counter++;
-    console.log(getStructureFromCsvRecord(data));
-    bulk.insert(getStructureFromCsvRecord(data));
-
-    if (counter % config.bulkRecordsLimit === 0) {
-      bulk.execute();
-      counter = 0;
-      bulk = db.collection.initializeUnorderedBulkOp();
-    }
+    console.log(data);
+    // bulk.insert(data);
+    //
+    // if (counter % config.bulkRecordsLimit === 0) {
+    //   bulk.execute();
+    //   counter = 0;
+    //   bulk = db.collection.initializeUnorderedBulkOp();
+    // }
   }
 });
 
-bulk.execute()
-  .then(() => {
-    console.log('Ok');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+reader.on('end', function() {
+
+  console.log('reader.end()');
+  //db.tmpCollectionName.renameCollection(collectionName, true);
+});
+
+// bulk.execute()
+//   .then(() => {
+//     console.log('Ok');
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
 insertFromCvsToMongoDb = (tmpCollectionName, collectionName) => {
   // logic here
 
-  db.tmpCollectionName.renameCollection('collectionName', true);
+
 };
 
-getStructureFromCsvRecord = (data) => {
-  return {
-    'LoanId': data[0],
-    'NoteId': data[1],
-    'OrderId': data[2],
-    'OutstandingPrincipal': data[3],
-    'AccruedInterest': data[4],
-    'Status': data[5],
-    'AskPrice': data[6],
-    'Markup/Discount': data[7],
-    'YTM': data[8],
-    'DaysSinceLastPayment': data[9],
-    'CreditScoreTrend': data[10],
-    'FICO End Range': data[11],
-    'Date/Time Listed': data[12],
-    'NeverLate': data[13],
-    'Loan Class': data[14],
-    'Loan Maturity': data[15],
-    'Original Note Amount': data[16],
-    'Interest Rate': data[17],
-    'Remaining Payments': data[18],
-    'Principal + Interest': data[19],
-    'Application Type': data[10]
-  };
+parse = (data) => {
+  return Object.assign({}, data, {
+    _fico: parseInt(data['FICO End Range'].split('-').shift()),
+    'Markup/Discount': parseFloat(data['Markup/Discount']),
+    DaysSinceLastPayment: parseInt(data.DaysSinceLastPayment),
+    NeverLate: data.NeverLate.toLowerCase() === 'true' ? true : false,
+    YTM: parseFloat(data.YTM)
+  })
 };
