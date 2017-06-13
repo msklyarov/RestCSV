0f8d6e26c0a59ba6a97d6fd2ebbf4941f03c0bea
Maxim Sklyarov <sklyarov.ma@gmail.com>
2017-06-13 08:18:05 +0300

    Commit 8:17 MSK

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  csvFileUrl: './',
+  csvFileUrl: './1.csv',
   mongoUrl: 'mongodb://192.168.99.100:27017/csv_data',
   timeout: 6000,
 };
--- csvServer.js
+++ csvServer.js
@@ -1,15 +1,17 @@
 const csv = require('ya-csv');
 const config = require('./config');
+const Promise = require('bluebird');
+const mongoClient = Promise.promisifyAll(require('mongodb').MongoClient);
 
 // temporary
 const fs = require('fs');
 const reader = csv.createCsvFileReader(config.csvFileUrl);
 
 let firstLine = true;
-// reader.addListener('data', (data) => {
-//   if (firstLine) {
-//     firstLine = false;
-//   } else {
-//     //console.log(data);
-//   }
-// });
+reader.addListener('data', (data) => {
+  if (firstLine) {
+    firstLine = false;
+  } else {
+    console.log(data);
+  }
+});
